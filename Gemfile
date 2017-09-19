cbe0dfe27f0d91ed1e88ca9aa1a688e96c18c6b2
Randy Fish <rfish@stanfordalumni.org>
2017-09-19 14:42:05 -0700

    CAT-42 1st pass at the flat file service writing to S3 with lookup into settings to check

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'string_enums'
 
+# Allows us to log to S3
+gem 'aws-sdk-s3', '~> 1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/services/api/inbound/flat_file_service.rb
+++ app/services/api/inbound/flat_file_service.rb
@@ -2,8 +2,54 @@ module API
   module Inbound
     class FlatFileService
       # TODO: implement flat file service for S3; rename class to reflect S3?
+
+      BASE_PATH = 'okl'.freeze
+
       def write_to_file(name:, ext: 'txt', data:)
-        File.open("tmp/#{name}.#{ext}", 'w') { |f| f.puts data }
+        filename = write_to_tmp(data, ext, name)
+
+        # looks like AWS wants the file written to disk (especially if it can be large)
+        enqueue_s3_write(ext, filename, name) if log_to_s3?
+      end
+
+      # TODO: put this operation on a queue
+      def enqueue_s3_write(ext, file, name)
+        @bucket_name ||= Rails.configuration.settings['inbound']['s3_bucket']
+
+        begin
+          s3_file_name = s3_file_name(source: BASE_PATH, name: name, ext: ext)
+          upload_to_s3(bucket: @bucket_name, name: s3_file_name, file: file)
+        rescue StandardError => e
+          puts 'Unable to upload to S3'
+          puts e.backtrace.join("\n")
+        end
+      end
+
+      def write_to_tmp(data, ext, name)
+        filename = "tmp/#{name}.#{ext}"
+        File.open(filename, 'w') { |f| f.puts data }
+        filename
+      end
+
+      private
+
+      def log_to_s3?
+        if Rails.configuration.settings.key?('inbound')
+          Rails.configuration.settings['inbound'].fetch('log_to_s3', false)
+        else
+          false
+        end
+      end
+
+      def s3_file_name(source:, name:, ext:)
+        day_str = Time.now.strftime('%Y%m%d')
+        "#{source}/#{day_str}/#{name}.#{ext}"
+      end
+
+      def upload_to_s3(bucket:, name:, file:)
+        s3  = Aws::S3::Resource.new
+        obj = s3.bucket(bucket).object(name)
+        obj.upload_file(file, acl: 'private')
       end
     end
   end
