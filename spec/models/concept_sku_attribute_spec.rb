6c8f22b9591d56d4cc089b147bd0484c20de127f
Todd <tkummer@onekingslane.com>
2017-09-20 12:06:54 -0700

    Fix names in specs

--- spec/models/concept_sku_attribute_spec.rb
+++ spec/models/concept_sku_attribute_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe CskuAttribute, type: :model do
+RSpec.describe ConceptSkuAttribute, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
