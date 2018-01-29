5a7312bd50685117c88892b2e9918d27bfbf4510
Randy Fish <rfish@stanfordalumni.org>
2018-01-29 11:46:21 -0800

    Add additional specs for the jobs

---
+++ spec/jobs/reindex/category_reindex_job_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe Reindex::CategoryReindexJob do
+  let(:job) { described_class.new }
+  let(:sku_indexer) { double(Indexer::SkuIndexer) }
+  let(:until_time) { DateTime.current }
+
+  let(:category_audit_datetime) { '2018-01-15'.to_datetime }
+  let(:sku_audit_datetime) { '2018-01-01'.to_datetime }
+
+  context '#changed_sku_ids' do
+    it 'should fetch changed ids' do
+      expect { job.changed_sku_ids(until_time) }.not_to raise_exception
+    end
+  end
+
+  it 'should return an index type of category' do
+    expect(job.index_type == 'category')
+  end
+
+  context '#start_time' do
+    before do
+      allow(Indexer::Audit).to receive(:last_successful_important_time).with('category')
+                                                                       .and_return(category_audit_datetime)
+      allow(Indexer::Audit).to receive(:last_successful_important_time).with('sku')
+                                                                       .and_return(sku_audit_datetime)
+    end
+    it 'should check the audit to retrieve the start time' do
+      expect(job.start_time).to eq(category_audit_datetime)
+    end
+
+    it 'should return sku_datetime if there is no category audit time' do
+      allow(Indexer::Audit).to receive(:last_successful_important_time).with('category').and_return(nil)
+
+      expect(job.start_time).to eq(sku_audit_datetime)
+    end
+  end
+end
