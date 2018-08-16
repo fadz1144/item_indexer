132e58d265d0661671fe01276793a48c404430f9
Todd <tkummer@onekingslane.com>
2018-08-16 11:29:47 -0700

    Refactor transformation service to expose engine
    This separates inbound-specific logic from the transformation logic.
    This will enable transformations from sources other than inbound tables.

--- app/services/transform/transformation_service.rb
+++ app/services/transform/transformation_service.rb
@@ -3,7 +3,7 @@ module Transform
   #
   # The transformation service processes inbound batches and pushes inserts / updates to the polished tables.
   class TransformationService
-    attr_writer :transformer_class
+    attr_writer :transformer
 
     def transform(inbound_batch_id)
       @batch = Transform::TransformBatchBuilderService.new.create_transform_batch_for_inbound_batch(inbound_batch_id)
@@ -15,53 +15,20 @@ module Transform
     private
 
     def process_inbound_batch
-      transformer_class.source_relation.where(inbound_batch_id: @batch.inbound_batch.id).find_in_batches do |group|
-        source_and_target_pairs(group) { |source, target| transform_item(source, target) }
-      end
-    end
-
-    def source_and_target_pairs(group)
-      tc = transformer_class
-      target_key = tc.target_match_key
-      source_key = tc.source_match_key
-      indexed_targets = tc.target_relation.where(target_key => group.map(&source_key)).index_by(&target_key)
-
-      group.each do |source|
-        yield(source, indexed_targets.fetch(source.public_send(source_key), tc.target_class.new))
-      end
-    end
-
-    def transform_item(source, target)
-      transformer = transformer_class.new(source)
-      transformer.apply_transformation(target)
-      save_item(source, target)
-    end
-
-    def save_item(source, target)
-      if target.valid?
-        target.save! if target.changed_for_autosave?
-      else
-        record_errors(source, target.errors.full_messages)
-      end
-    rescue => e
-      Rails.logger.error "[#{self.class}] Unexpected error saving item: #{e.message}\n\t#{e.backtrace}"
-      record_errors(source, [e.message])
-    end
-
-    def record_errors(source, error_messages)
-      error_messages.each do |error_message|
-        @batch.batch_errors.build(source_item: source, message: error_message)
+      engine = CatalogTransformer::Engine.new(@batch, transformer)
+      transformer.source_relation.where(inbound_batch_id: @batch.inbound_batch.id).find_in_batches do |source_records|
+        engine.transform_items(source_records)
       end
     end
 
     # can be derived or injected
-    def transformer_class
-      @transformer_class ||= determine_transformer_class
+    def transformer
+      @transformer ||= determine_transformer
     end
 
     # convention is that these are concept-specific transformations; this means, for example, that if a transformer is
     # needed for ConceptSkuPricing, then it should come in under the data type sku_pricing
-    def determine_transformer_class
+    def determine_transformer
       source = @batch.inbound_batch.source
       data_type = @batch.inbound_batch.data_type
       "Transform::Transformers::#{source.upcase}::Concept#{data_type.titlecase}".constantize
---
+++ app/transformers/catalog_transformer/engine.rb
@@ -0,0 +1,52 @@
+module CatalogTransformer
+  # = Catalog Transformer Engine
+  #
+  # The Engine class transforms source data into target records. There are two entry points:
+  # - use transform_items with a batch of source records to let the engine load existing target data
+  # - use transform_item to supply both the source record and the target record
+  #
+  # Errors will be recorded to the transform batch. The engine does NOT manage the status on the transform batch.
+  class Engine
+    def initialize(batch, transformer_class)
+      @batch = batch
+      @transformer_class = transformer_class
+    end
+
+    def transform_items(source_records)
+      target_key = @transformer_class.target_match_key
+      source_key = @transformer_class.source_match_key
+      indexed_targets =
+        @transformer_class.target_relation.where(target_key => source_records.map(&source_key)).index_by(&target_key)
+
+      source_records.each do |source|
+        transform_item(source,
+                       indexed_targets.fetch(source.public_send(source_key), @transformer_class.target_class.new))
+      end
+    end
+
+    def transform_item(source, target)
+      transformer = @transformer_class.new(source)
+      transformer.apply_transformation(target)
+      save_item(source, target)
+    end
+
+    private
+
+    def save_item(source, target)
+      if target.valid?
+        target.save! if target.changed_for_autosave?
+      else
+        record_errors(source, target.errors.full_messages)
+      end
+    rescue => e
+      Rails.logger.error "[#{self.class}] Unexpected error saving item: #{e.message}\n\t#{e.backtrace}"
+      record_errors(source, [e.message])
+    end
+
+    def record_errors(source, error_messages)
+      error_messages.each do |error_message|
+        @batch.batch_errors.build(source_item: source, message: error_message)
+      end
+    end
+  end
+end
