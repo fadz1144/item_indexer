894a572eaca7a37d3f2d789b394297b1382e816a
Daniel Pritchard <dpritchard@onekingslane.com>
2018-08-30 10:16:34 -0700

    Fix name in hb deploy notify command

--- bin/server.aws.sh
+++ bin/server.aws.sh
@@ -9,7 +9,7 @@ echo "DB create complete. About to start db:migrate"
 bundle exec rails db:migrate
 echo 'DB actions complete.'
 echo 'Reporting deployment in Honeybadger...'
-/okl/app/bin/deploy_notify.sh search_api /okl/app || true
+/okl/app/bin/deploy_notify.sh item_indexer /okl/app || true
 echo 'Running puma...'
 export RAILS_RELATIVE_URL_ROOT='/item_indexer'
 bundle exec puma -C config/puma.rb -b unix:///var/run/puma.sock
--- bin/server.sh
+++ bin/server.sh
@@ -13,7 +13,7 @@ bundle exec rails db:version
 echo 'DB actions complete.'
 if [[ "$RACK_ENV" = "production" ]] || [[ "$RAILS_ENV" = "production" ]] ; then
     echo 'Reporting deployment in Honeybadger...'
-    /bbb/app/bin/deploy_notify.sh search_api /bbb/app || true
+    /bbb/app/bin/deploy_notify.sh item_indexer /bbb/app || true
 
     echo 'Configure App Engine Firewall (Ensuring all instances are allowed for RPC purposes)...'
     bundle exec rake bridge_cloud:app_engine:add_all_instances_to_firewall
