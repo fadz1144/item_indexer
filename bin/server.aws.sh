31b007dbe12891c2cb5683c782b1bed068dd5efe
Daniel Pritchard <dpritchard@onekingslane.com>
2018-08-30 10:28:08 -0700

    Fixing /okl/ -> /bbb/ paths to be consistent across both aws and gcp versions

--- bin/server.aws.sh
+++ bin/server.aws.sh
@@ -2,14 +2,14 @@
 set -e
 nginx
 echo "nginx started"
-rm -f /okl/app/tmp/pids/puma.pid
+rm -f /bbb/app/tmp/pids/puma.pid
 echo "About to start db:create"
 bundle exec rails db:create
 echo "DB create complete. About to start db:migrate"
 bundle exec rails db:migrate
 echo 'DB actions complete.'
 echo 'Reporting deployment in Honeybadger...'
-/okl/app/bin/deploy_notify.sh item_indexer /okl/app || true
+/bbb/app/bin/deploy_notify.sh item_indexer /bbb/app || true
 echo 'Running puma...'
 export RAILS_RELATIVE_URL_ROOT='/item_indexer'
 bundle exec puma -C config/puma.rb -b unix:///var/run/puma.sock
