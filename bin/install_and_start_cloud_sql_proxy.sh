ed76e29e1e87d342476e6a6ac93f5038bb495225
Daniel Pritchard <dpritchard@onekingslane.com>
2018-09-07 13:24:19 -0700

    Add credentials as secrets mount

--- bin/install_and_start_cloud_sql_proxy.sh
+++ bin/install_and_start_cloud_sql_proxy.sh
@@ -4,7 +4,8 @@ CS_INSTANCE="$1"
 echo "Starting Cloud SQL Proxy..."
 curl -o cloud_sql_proxy "https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64"
 chmod +x cloud_sql_proxy
-./cloud_sql_proxy -dir=/cloudsql -instances="${CS_INSTANCE}" &
+mkdir /cloudsql && chmod 777 /cloudsql
+./cloud_sql_proxy -dir=/cloudsql -instances="${CS_INSTANCE}" -credential_file=/secrets/cloudsql/credentials.json &
 sleep 3
 echo "Cloud SQL Proxy started."
 DATABASE_HOST="/cloudsql/${CS_INSTANCE}"
