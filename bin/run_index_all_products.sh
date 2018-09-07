80657de42716111bb1afe22a6e8909140792f3be
Daniel Pritchard <dpritchard@onekingslane.com>
2018-09-07 09:54:27 -0700

    allow product index task to be sensitive to alternative search engine lifestyles

--- bin/run_index_all_products.sh
+++ bin/run_index_all_products.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-bundle exec rake bridge:build_product_search_index
+if [[ "$INDEX_SEARCH_ENGINE" == "solr" ]] ; then
+    bundle exec rake bridge:build_solr_product_search_index
+else
+    bundle exec rake bridge:build_product_search_index
+fi
