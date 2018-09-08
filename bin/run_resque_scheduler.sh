46fe893cccba51e4c70884c20d1d778f3e537f84
Daniel Pritchard <dpritchard@onekingslane.com>
2018-09-08 00:27:39 -0700

    Add firewall config rule to resque scheduler so it can reach redis

--- bin/run_resque_scheduler.sh
+++ bin/run_resque_scheduler.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+echo 'Configure App Engine Firewall (Ensuring all instances are allowed for RPC purposes)...'
+bundle exec rake bridge_cloud:app_engine:add_all_instances_to_firewall
+echo "Running resque scheduler task..."
 bundle exec rails resque:scheduler
