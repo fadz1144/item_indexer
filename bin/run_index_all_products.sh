b9df987d7ec7330da6799ffc46c1e2bee9229076
Daniel Pritchard <dpritchard@onekingslane.com>
2018-09-09 14:17:49 -0700

    Use internal-to-vpc solr endpoint where we can

--- bin/run_index_all_products.sh
+++ bin/run_index_all_products.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 if [[ "$INDEX_SEARCH_ENGINE" == "solr" ]] ; then
+    export SOLR_ENDPOINT=$SOLR_INTERNAL_ENDPOINT
+    env | fgrep SOLR
     bundle exec rake bridge:build_solr_product_search_index
 else
     bundle exec rake bridge:build_product_search_index
