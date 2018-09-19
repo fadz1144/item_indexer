a6652c242263ef8ccf567758e2756e232443dd51
Todd <tkummer@onekingslane.com>
2018-09-19 09:21:35 -0700

    FIX: TransformationJob bogus lock error

--- app/jobs/transform/transformation_job.rb
+++ app/jobs/transform/transformation_job.rb
@@ -4,18 +4,24 @@ module Transform
 
     def perform(source)
       lock_name = "#{self.class.name}:#{source}"
+      with_lock(lock_name) { run_service(source) }
+    end
 
+    private
+
+    def with_lock(lock_name)
       mutex = RedisSimpleMutex.new(lock_name)
+      lock_acquired = false
+
       if mutex.lock
-        run_service(source)
+        lock_acquired = true
+        yield
       else
         Rails.logger.info "Failed to acquire lock for '#{lock_name}'"
       end
     ensure
-      mutex.unlock
+      mutex.unlock if lock_acquired
     end
-
-    private
 
     def run_service(source)
       each_inbound_batch_id(source) do |inbound_batch_id|
