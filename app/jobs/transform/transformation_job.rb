b1e556c8d4c77dbf7d1ba5b0565d245f33858ba4
Daniel Pritchard <dpritchard@onekingslane.com>
2018-10-13 13:42:11 -0700

    Add logging to TransformationJob

--- app/jobs/transform/transformation_job.rb
+++ app/jobs/transform/transformation_job.rb
@@ -9,12 +9,14 @@ module Transform
 
     private
 
+    # rubocop:disable Metrics/MethodLength
     def with_lock(lock_name)
       mutex = RedisSimpleMutex.new(lock_name)
       lock_acquired = false
 
       if mutex.lock
         lock_acquired = true
+        Rails.logger.info "Got the '%s' lock, ready to go!" % lock_name
         yield
       else
         Rails.logger.info "Failed to acquire lock for '#{lock_name}'"
@@ -22,15 +24,20 @@ module Transform
     ensure
       mutex.unlock if lock_acquired
     end
+    # rubocop:enable Metrics/MethodLength
 
     def run_service(source)
+      Rails.logger.info 'Looking for batches in need of transformation...'
       each_inbound_batch_id(source) do |inbound_batch_id|
+        Rails.logger.info "Transforming inbound batch ID: #{inbound_batch_id}"
         Transform::TransformationService.new.transform(inbound_batch_id)
       end
+      Rails.logger.info 'Completed all the above transformations.'
     end
 
     def each_inbound_batch_id(source)
       relation = Inbound::Batch.ready_for_transformation.where(source: source).order(:inbound_batch_id)
+      Rails.logger.info("There are #{relation.length} batches to transform.")
       loop do
         id = relation.reload.first&.inbound_batch_id
         break if id.nil?
--- lib/tasks/transformers.rake
+++ lib/tasks/transformers.rake
@@ -1,7 +1,8 @@
 namespace :bridge do
   desc 'Builds the product index for (Bridge) Catalog'
   task 'run_transformation_job', [:source] => :environment do |_task, args|
-    source = args[:source].upcase
+    # Following ensures no quotes accidentally get in from being quoted without a shell decoding them
+    source = args[:source].gsub(/['"]/, '').upcase
     puts "Running transformers for source: #{source}"
     job = Transform::TransformationJob.new
     job.perform(source)
