50720e6d77c7e7d5cc07ff4cda1acbe63b48192c
Todd <tkummer@onekingslane.com>
2018-10-25 11:19:05 -0700

    Transformers: OKL product membership and merch trees
    - change the product membership transformation to allow for membership
    with non-OKL products
    - comment out the merch tree nodes in the OKL sku transformer; they
    were clearing data from shared skus

--- app/transformers/transform/transformers/okl/product_membership.rb
+++ app/transformers/transform/transformers/okl/product_membership.rb
@@ -4,6 +4,11 @@ module Transform
       class ProductMembership < CatalogTransformer::Base
         source_name 'Inbound::OKL::SkuRevision'
         references :product, association: :concept_product
+
+        # enables the source product Id to be used as the match key
+        def attribute_values
+          super.merge('product_id' => @source.concept_product&.product&.product_id)
+        end
       end
     end
   end
--- app/transformers/transform/transformers/okl/sku.rb
+++ app/transformers/transform/transformers/okl/sku.rb
@@ -5,21 +5,24 @@ module Transform
         source_name 'Inbound::OKL::SkuRevision'
         decorator_name 'Transform::Transformers::OKL::Decorators::SkuConceptSkuDecorator'
 
-        has_many :product_memberships, match_keys: [:sku_id], partial: true
+        has_many :product_memberships, match_keys: [:product_id], partial: true
         attribute :sku_id, source_name: :sku_id
         attribute :gtin, source_name: :upc
 
         references :vendor, association: :concept_vendor
         references :brand, association: :concept_brand
         references :category, association: :concept_category
-        references :merch_dept_tree_node
-        references :merch_sub_dept_tree_node
-        references :merch_class_tree_node
+
+        # TODO: does the publish from OKL include the keys for these?
+        # references :merch_dept_tree_node
+        # references :merch_sub_dept_tree_node
+        # references :merch_class_tree_node
 
         exclude :eph_tree_node_id, :chain_status, :ecom_status, :rollup_type_cd, :rollup_type_name,
                 :tbs_blocked_reason_cd, :tbs_blocked_reason_name, :available_in_ca_dist_cd, :transferable_to_canada,
                 :ca_fulfillment_cd, :ca_fulfillment_name, :vdc_sku, :jda_description, :pos_description,
                 :units_sold_last_1_week, :units_sold_last_4_weeks, :units_sold_last_8_weeks, :units_sold_last_52_weeks,
+                :merch_dept_tree_node_id, :merch_sub_dept_tree_node_id, :merch_class_tree_node_id,
                 allow_primary_key: true
 
         module Decorations
