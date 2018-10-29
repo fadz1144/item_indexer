38317e348fe8071e053c7691029eb43fea475f8b
Daniel Pritchard <dpritchard@onekingslane.com>
2018-10-29 11:24:55 -0700

    Fix syntax error 🤦‍♂️

--- bin/run_index_all_products.sh
+++ bin/run_index_all_products.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [[ "$INDEX_SEARCH_ENGINE" == "es" ]] ; then
     echo "ES not supported anymore. Only SOLR"
-    exit(1)
+    exit 1
 else
     export SOLR_ENDPOINT=$SOLR_INTERNAL_ENDPOINT
     env | fgrep SOLR
