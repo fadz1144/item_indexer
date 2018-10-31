a1d4e33838697bc35be22cd35fb5523b5d969d06
Todd <tkummer@onekingslane.com>
2018-10-31 10:48:03 -0700

    New class SOLR::FieldDefinition
    Enables additional information to be gathered in the field definitions.

---
+++ spec/serializers/solr/field_definition_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe SOLR::FieldDefinition do
+  context 'solr_field_definition' do
+    it 'includes specified options' do
+      options = { type: 'bears', indexed: true, stored: true, multiValued: false }
+      f = described_class.new('oski', options)
+      expect(f.solr_field_definition).to match(options.merge(name: 'oski'))
+    end
+
+    it 'excludes non-solr options' do
+      options = { type: 'bears', blue: true }
+      f = described_class.new('oski', options)
+      expect(f.solr_field_definition.keys).not_to include(:blue)
+    end
+
+    it 'provides default for indexed' do
+      options = { type: 'bears' }
+      f = described_class.new('oski', options)
+      expect(f.solr_field_definition).to include(indexed: true)
+    end
+
+    it 'provides default for stored' do
+      options = { type: 'bears' }
+      f = described_class.new('oski', options)
+      expect(f.solr_field_definition).to include(stored: true)
+    end
+  end
+
+  it '#name' do
+    options = { type: 'bears' }
+    f = described_class.new('oski', options)
+    expect(f.name).to eq :oski
+  end
+
+  context '#source_name' do
+    it 'falls back to name when not specified' do
+      options = { type: 'bears' }
+      f = described_class.new('oski', options)
+      expect(f.source_name).to eq :oski
+    end
+
+    it 'returns source name when specified' do
+      options = { type: 'bears', source_name: 'bear' }
+      f = described_class.new('oski', options)
+      expect(f.source_name).to eq :bear
+    end
+  end
+end
