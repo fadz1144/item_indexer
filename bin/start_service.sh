20a6186341a6d44e2587c18bb0d54adcb4f4873d
Todd <tkummer@onekingslane.com>
2019-01-18 14:28:10 -0800

    Remove SQL Proxy and Kube Redis from start_service.sh

--- bin/start_service.sh
+++ bin/start_service.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -n "${CLOUD_SQL_PROXY_INSTANCE}" ]] ; then
-    bin/install_and_start_cloud_sql_proxy.sh "${CLOUD_SQL_PROXY_INSTANCE}"
-    source dbhost.sh
-    env | grep DATABASE_HOST
-fi
-
-if [[ "${RUNTIME_ENV}" == "kube" ]] ; then
-    echo '[Deployment] Swapping Redis to internal IP because we are running in K8'
-    export REDIS_HOST=${REDIS_INTERNAL_HOST}
-fi
-
 # used to run resque worker and resque scheduler
 if [[ -n "${OVERRIDE_COMMAND}" ]] ; then
     echo "[Deployment] *** About to run override command: ${OVERRIDE_COMMAND}"
@@ -20,10 +9,11 @@ if [[ -n "${OVERRIDE_COMMAND}" ]] ; then
     exit 0
 fi
 
+# the deployment scripts come from the deployment-scripts image which is built in the bridge-deploy repo (see COPY
+# command in Dockerfile)
 bin/start_nginx.sh
 bin/report_deployment_to_honeybadger.sh item_indexer /bbb/app || true
 bin/configure_gae_firewall.sh
 bin/prepare_database_rake.sh
 export RAILS_RELATIVE_URL_ROOT='/item_indexer'
 bin/start_puma.sh
-
