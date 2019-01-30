a8236221778c7d421687d215d04ab91f8ac7a4e0
Daniel Pritchard <dpritchard@onekingslane.com>
2019-01-29 16:03:42 -0800

    CAT-1083, CAT-1084: Add inbound table for CM feed.
    Adds inbound table for contribution margin feed.

---
+++ app/models/inbound/dw/contribution_margin_feed.rb
@@ -0,0 +1,34 @@
+module Inbound
+  module DW
+    class ContributionMarginFeed < ApplicationRecord
+      def self.table_name_prefix
+        'inbound_dw_'
+      end
+
+      def self.table_name ; 'inbound_dw_contribution_margin_feed' ; end
+
+      SITE_ID_TO_CONCEPT = {
+        'BedBathUS' => 1,
+        'BedBathCA' => 2, # A TOTAL GUESS - NEVER SEEN ANY OTHER VALUE BUT BedBathUS
+        'BuyBuyBaby' => 4, # A TOTAL GUESS - NEVER SEEN ANY OTHER VALUE BUT BedBathUS
+      }.freeze
+
+      def concept_id
+        SITE_ID_TO_CONCEPT[site_id] || (raise site_id_error_msg(site_id))
+      end
+
+      def compound_source_key
+        '%d:%d' % [concept_id, sku_id]
+      end
+
+      private
+
+      def site_id_error_msg(site_id)
+        'Unrecognized value in SITE_ID field from mft: %d. We only expect values in (%s)' % [
+          site_id,
+          SITE_ID_TO_CONCEPT.keys.join('|')
+        ]
+      end
+    end
+  end
+end
