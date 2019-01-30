f66b98ba55a092e3fbea2d446a05104cc9ee79a7
Todd <tkummer@onekingslane.com>
2019-01-29 17:03:19 -0800

    CAT-1084: Exclude new CM pricing fields from existing transformers

--- app/transformers/transform/transformers/jda/concept_sku_pricing.rb
+++ app/transformers/transform/transformers/jda/concept_sku_pricing.rb
@@ -14,7 +14,12 @@ module Transform
         attribute :source_updated_at, source_name: :DPCRTDT
         exclude :concept_sku_id, :concept_id, :source_sku_id, :cost,
                 :margin_amount, :margin_percent, :source_created_by, :source_created_at, :source_updated_by,
-                :contribution_margin_amount, :contribution_margin_percent
+                :contribution_margin_amount, :contribution_margin_percent, :cm_amount_sum_l4w, :cm_rate_l4w,
+                :cm_sales_units_sum_l4w, :cm_retail_sales_sum_l4w, :cm_coupon_sum_l4w, :cm_sales_cost_sum_l4w,
+                :cm_freight_in_sum_l4w, :cm_freight_out_sum_l4w, :cm_shipping_paid_by_customer_sum_l4w,
+                :cm_shrink_sum_l4w, :cm_net_damages_sum_l4w, :cm_marked_out_of_stock_sum_l4w,
+                :cm_markdown_reimbursement_sum_l4w, :cm_vendor_funded_allowances_sum_l4w, :cm_net_sales_retail_sum_l4w,
+                :cm_cost_sum_l4w
 
         def self.source_relation
           super.order(:DPCRTDT)
