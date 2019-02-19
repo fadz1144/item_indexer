5e7a2925d22451b17c86fc88d7b9130517d23351
Todd Kummer <toddkummer@users.noreply.github.com>
2019-02-18 16:16:50 -0800

    Do not set price during XPDM transformation (#187)

--- app/transformers/transform/transformers/xpdm/concept_sku_pricing.rb
+++ app/transformers/transform/transformers/xpdm/concept_sku_pricing.rb
@@ -5,19 +5,16 @@ module Transform
         source_name 'External::XPDM::Sku'
         attribute :sku_id, source_name: :pdm_object_id
         attribute :source_sku_id, source_name: :pdm_object_id
-        attribute :retail_price, source_name: :price
         references :concept
 
-        exclude :concept_sku_id, :pre_markdown_price, :map_price, :contribution_margin_amount,
+        # retail_price, margin_amount, and margin_percent are populated by JDA concept sku pricing transformation
+        exclude :concept_sku_id, :retail_price, :pre_markdown_price, :map_price, :contribution_margin_amount,
                 :contribution_margin_percent, :cm_amount_sum_l4w, :cm_rate_l4w, :cm_sales_units_sum_l4w,
                 :cm_retail_sales_sum_l4w, :cm_coupon_sum_l4w, :cm_sales_cost_sum_l4w, :cm_freight_in_sum_l4w,
                 :cm_freight_out_sum_l4w, :cm_shipping_paid_by_customer_sum_l4w, :cm_shrink_sum_l4w,
                 :cm_net_damages_sum_l4w, :cm_marked_out_of_stock_sum_l4w, :cm_markdown_reimbursement_sum_l4w,
-                :cm_vendor_funded_allowances_sum_l4w, :cm_net_sales_retail_sum_l4w, :cm_cost_sum_l4w
-
-        module Decorations
-          include Transform::Transformers::Margin
-        end
+                :cm_vendor_funded_allowances_sum_l4w, :cm_net_sales_retail_sum_l4w, :cm_cost_sum_l4w,
+                :margin_amount, :margin_percent
       end
     end
   end
