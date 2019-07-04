89a9c56d3d8e4579f2377d0f5131dae1a2da9877
Aman Kumar <aman.kumar@idc.bedbath.com>
2019-07-04 16:49:12 +0530

    change spacing for tbs_blocked date and revert web copy complete code (#228)

--- app/transformers/transform/transformers/xpdm/sku.rb
+++ app/transformers/transform/transformers/xpdm/sku.rb
@@ -34,7 +34,6 @@ module Transform
         attribute :prop_65_chemicals, association: :compliance, source_name: :list_prop65_chem_txt
         attribute :tbs_blocked_start_date, source_name: :blck_start_dt, association: :web_info
         attribute :tbs_blocked_end_date, source_name: :blck_end_dt, association: :web_info
-        attribute :web_copy_complete_status, source_name: :web_copy_cmplt_ind, association: :web_info
         attribute :vdc_shipping_cutoff_offset, source_name: :vdc_shp_ctoff_offst_day_cnt
 
         # TODO: unit of measure
