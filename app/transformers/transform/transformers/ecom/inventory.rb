e8e0ffcf1fe4b0ba3e6251b7b43c7122fa3cbe6d
Manish Arora <manish.arora@idc.bedbath.com>
2020-01-06 19:50:40 +0530

    Inventory Calculation Fix

--- app/transformers/transform/transformers/ecom/inventory.rb
+++ app/transformers/transform/transformers/ecom/inventory.rb
@@ -7,12 +7,15 @@ module Transform
         end
 
         def qty_attribute_values(concept_id)
-          qty = @source.afs_qty + @source.concept_quantity(concept_id)
-
+          qty = @source.afs_qty + @source.concept_quantity(concept_id) + @source.concept_igr_qty(concept_id)
           { 'total_avail_qty' => qty,
             'warehouse_avail_qty' => @source.warehouse? ? @source.afs_qty : 0,
-            'stores_avail_qty' => @source.warehouse? ? @source.concept_quantity(concept_id) : 0,
-            'vdc_avail_qty' => @source.vdc? ? qty : 0 }
+            'stores_avail_qty' => if @source.warehouse?
+                                    @source.concept_quantity(concept_id) + @source.concept_igr_qty(concept_id)
+                                  else
+                                    0
+                                  end,
+            'vdc_avail_qty' => @source.vdc? ? @source.afs_qty : 0 }
         end
 
         module Decorations
@@ -24,6 +27,19 @@ module Transform
               ca_alt_afs_qty
             when 4
               bab_alt_afs_qty
+            else
+              raise "Unknown concept_id (#{concept_id}) (#{target.sku_id}) for ECOM Inventory"
+            end
+          end
+
+          def concept_igr_qty(concept_id)
+            case concept_id
+            when 1
+              bbb_igr_qty
+            when 2
+              ca_igr_qty
+            when 4
+              bab_igr_qty
             else
               raise "Unknown concept_id (#{concept_id}) for ECOM Inventory"
             end
