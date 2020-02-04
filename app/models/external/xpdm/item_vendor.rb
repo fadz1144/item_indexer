02f1146ae996c275dc4fe6954816c0e48178fd0d
Aman Kumar <aman.kumar@idc.bedbath.com>
2020-02-05 01:58:06 +0530

    fix errors

--- app/models/external/xpdm/item_vendor.rb
+++ app/models/external/xpdm/item_vendor.rb
@@ -3,7 +3,11 @@ module External
     class ItemVendor < External::XPDM::Base
       self.table_name = 'pdm_item_prmry_vdr_info'
       self.primary_key = 'pdm_object_id'
-      default_scope -> { where.not(pmry_vdr_num: 0) }
+      default_scope { by_vdr_num.by_newest }
+      scope :vdr_num_not_zero, -> { where.not(pmry_vdr_num: 0) }
+      scope :by_newest, -> { order('update_ts DESC') }
+      scope :by_vdr_num, -> { order('pmry_vdr_num DESC') }
+
       belongs_to :item, foreign_key: :pdm_object_id, primary_key: :pdm_object_id, inverse_of: :item_vendor
       belongs_to :concept_vendor, -> { where(concept_id: 99) },
                  optional: true,
